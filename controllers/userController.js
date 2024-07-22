e01a60c262dde9e9a880e8f0e128b8685ba1bf9d
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-07-21 21:12:08 -0500

    added functionality to add and remove friends to user's profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,12 +66,46 @@ module.exports = {
             }
 
             await Thought.deleteMany({ _id: { $in: user.thoughts } });
-            res.status(200).json({ message: `${user.userName} has been deleted`})
+            res.status(200).json({ message: `${user.username} has been deleted`})
         } catch (error) {
             res.status(500).json(error);
         }
     },
 
     // Add a friend
-     
+    async addFriend(req,res) {
+        try {
+            const user = await User.findByIdAndUpdate(
+                req.params.userId,
+                { $addToSet: { friends: req.params.friendId } },
+                {new: true}
+            )
+
+            if (!user) {
+                res.status(404).json({ message: "No user found with that id"} )
+            }
+
+            res.json(user);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+    // Delete a friend
+    async deleteFriend(req,res) {
+        try {
+            const user = await User.findByIdAndUpdate(
+                req.params.userId,
+                { $pull: { friends: req.params.friendId } },
+                {new: true}
+            )
+
+            if (!user) {
+                res.status(404).json({ message: "No user found with that id"} )
+            }
+            res.status(200).json({ message: `Friend has been deleted`})
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
 }
